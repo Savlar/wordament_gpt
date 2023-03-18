d5a9dcc8796a6a57567b7421473415aac494ece6
Savlar <matusgal@outlook.com>
2023-03-18 14:22:17 +0100

    added cors

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from wordament_solver import find_words
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
 
 @app.route('/api/find-words', methods=['POST'])
+@cross_origin()
 def api_find_words():
     matrix = request.json.get('matrix')
     n = request.json.get('n')
