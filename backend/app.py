77feb80e46fb63d112a9f65d624b8e7e343d1884
Savlar <matusgal@outlook.com>
2023-03-18 13:44:18 +0100

    removed cors

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,8 @@
 from flask import Flask, request, jsonify
 from wordament_solver import find_words
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-app.debug = True
+
 
 @app.route('/api/find-words', methods=['POST'])
 def api_find_words():
@@ -14,6 +11,3 @@ def api_find_words():
     words = find_words(matrix, n)
     return jsonify(words=words)
 
-
-if __name__ == '__main__':
-    app.run()
